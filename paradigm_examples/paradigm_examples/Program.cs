b6da361c904505ba82f2c3679cdb060bcfe60aee
Thisarani Pathiranage <137994537+ThisaraniAP@users.noreply.github.com>
2024-12-25 01:33:49 +0530

    Add files via upload

---
+++ paradigm_examples/paradigm_examples/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace paradigm_examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std1 = new Student("Harry", "Potter", 11, "Male", 75);
+            Student std2 = new Student("Hermione", "Granger", 11, "Female", 100);
+
+            std1.Information();
+            std2.Information();
+        }
+    }
+}
+
---
+++ paradigm_examples/paradigm_examples/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paradigm_examples
+{
+    internal class Student
+    {
+        private string name;
+        private int age;
+        private string gender;
+        private int marks;
+
+        public Student(string firstName, string lastName, int age, string gender, int marks)
+        {
+            this.name = $"{firstName} {lastName}";
+            this.age = age;
+            this.gender = gender;
+            this.marks = marks;
+        }
+
+        public void Information()
+        {
+            Console.WriteLine($"Name: {this.name} \nAge: {this.age}\nGender: {this.gender}" +
+                $"\nMarks: {this.marks}");
+        }
+    }
+}
+
