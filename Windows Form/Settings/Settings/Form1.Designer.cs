7e34aebb01be3a947495f06f154a91ce5bccd95e
Thisarani Pathiranage <137994537+ThisaraniAP@users.noreply.github.com>
2024-12-25 01:50:12 +0530

    Add files via upload

---
+++ Windows Form/Settings/Settings/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Settings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDateRange = new System.Windows.Forms.TextBox();
+            this.txtNoOfLeaves = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dtBeginDate = new System.Windows.Forms.DateTimePicker();
+            this.dtEndDate = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DateRange";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BeginDate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "EndDate";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "No of Leaves";
+            // 
+            // txtDateRange
+            // 
+            this.txtDateRange.Location = new System.Drawing.Point(122, 50);
+            this.txtDateRange.Name = "txtDateRange";
+            this.txtDateRange.Size = new System.Drawing.Size(118, 20);
+            this.txtDateRange.TabIndex = 4;
+            // 
+            // txtNoOfLeaves
+            // 
+            this.txtNoOfLeaves.Location = new System.Drawing.Point(122, 153);
+            this.txtNoOfLeaves.Name = "txtNoOfLeaves";
+            this.txtNoOfLeaves.Size = new System.Drawing.Size(118, 20);
+            this.txtNoOfLeaves.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(15, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Settings";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.button1.Location = new System.Drawing.Point(165, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dtBeginDate
+            // 
+            this.dtBeginDate.Location = new System.Drawing.Point(122, 83);
+            this.dtBeginDate.Name = "dtBeginDate";
+            this.dtBeginDate.Size = new System.Drawing.Size(118, 20);
+            this.dtBeginDate.TabIndex = 10;
+            // 
+            // dtEndDate
+            // 
+            this.dtEndDate.Location = new System.Drawing.Point(122, 118);
+            this.dtEndDate.Name = "dtEndDate";
+            this.dtEndDate.Size = new System.Drawing.Size(118, 20);
+            this.dtEndDate.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 194);
+            this.Controls.Add(this.dtEndDate);
+            this.Controls.Add(this.dtBeginDate);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtNoOfLeaves);
+            this.Controls.Add(this.txtDateRange);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDateRange;
+        private System.Windows.Forms.TextBox txtNoOfLeaves;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dtBeginDate;
+        private System.Windows.Forms.DateTimePicker dtEndDate;
+    }
+}
+
---
+++ Windows Form/Settings/Settings/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Settings
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=LAB1-PC-22;Initial Catalog=employee;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void View()
+        {
+            try
+            {
+                string sqlsearch;
+                sqlsearch = "Select * from Settings";
+                SqlCommand cmd = new SqlCommand(sqlsearch, con);
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                txtDateRange.Text = dr["DateRangein"].ToString();
+                dtBeginDate.Text = dr["BeginDate"].ToString();
+                dtEndDate.Text = dr["EndDate"].ToString();
+                txtNoOfLeaves.Text = dr["NoOfLeaves"].ToString();
+                con.Close();
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+                con.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sqlUpdate = "update Settings set DateRangein = '" + txtDateRange.Text + "', BeginDate = '" + dtBeginDate.Value + "', EndDate = '" + dtEndDate.Value + "', NoOfLeaves = '" + txtNoOfLeaves.Text + "'";
+                SqlCommand cmd = new SqlCommand(sqlUpdate, con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Settings updated.");
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+                con.Close();
+            }
+        }
+    }
+}
